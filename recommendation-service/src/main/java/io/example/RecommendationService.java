310251a1a0cf6512475d653bb67839ea923adf9e
iBranch211 <leo.esaki211@gmail.com>
2018-12-07 22:38:19 +0100

    Add some data.

--- recommendation-service/src/main/java/io/example/RecommendationService.java
+++ recommendation-service/src/main/java/io/example/RecommendationService.java
@@ -2,9 +2,7 @@ package io.example;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 /**
  * This event stream processor monitors a stream of events from multiple microservices and builds an eventually
@@ -13,8 +11,6 @@ import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
  * @author Kenny Bastani
  */
 @SpringBootApplication
-@EnableNeo4jRepositories(value = {"io.example.domain.user", "io.example.domain.friend"})
-@EntityScan({"io.example.domain.friend.entity", "io.example.domain.user.entity"})
 public class RecommendationService {
 
     public static void main(String[] args) {
