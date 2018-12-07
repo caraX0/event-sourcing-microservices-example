d0ac135505efdc9f1e270ca035d26dfffe49a395
iBranch211 <leo.esaki211@gmail.com>
2018-12-07 21:59:04 +0100

    Simplify configuration of SDN and OGM.

--- recommendation-service/src/main/java/io/example/domain/friend/FriendController.java
+++ recommendation-service/src/main/java/io/example/domain/friend/FriendController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/recommendation")
 public class FriendController {
 
     private final FriendRepository friendRepository;
@@ -16,12 +16,12 @@ public class FriendController {
         this.friendRepository = friendRepository;
     }
 
-    @GetMapping(path = "/friends/{userId}/commands/findMutualFriends")
+    @GetMapping(path = "/users/{userId}/commands/findMutualFriends")
     List<User> getMutualFriends(@PathVariable Long userId, @RequestParam Long friendId) {
         return friendRepository.mutualFriends(userId, friendId);
     }
 
-    @GetMapping(path = "/friends/{userId}/commands/recommendFriends")
+    @GetMapping(path = "/users/{userId}/commands/recommendFriends")
     List<RankedUser> recommendFriends(@PathVariable Long userId) {
         return friendRepository.recommendedFriends(userId);
     }
